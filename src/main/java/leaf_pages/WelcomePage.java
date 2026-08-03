33a73bf170415e4eb15707cdb4214f88d2cf7810
Manoj-NS <manoj.u2907@gmail.com>
2026-08-04 01:29:07 +0530

    updated the code

---
+++ src/main/java/leaf_pages/WelcomePage.java
@@ -0,0 +1,50 @@
+package leaf_pages;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WelcomePage {
+	
+	WebDriver driver;
+	WebDriverWait wait;
+	
+//Constructor	
+	public WelcomePage(WebDriver driver) {
+		this.driver=driver;
+		this.wait=new WebDriverWait(driver, Duration.ofSeconds(15));
+		PageFactory.initElements(driver, this);
+	}
+	
+//Locators
+	
+	@FindBy(xpath="//h2[normalize-space(.)='Welcome Demo B2B CSR']")
+	WebElement welcomeText;
+	
+	@FindBy(id="logout")
+	WebElement logoutBtn;
+	
+	@FindBy(xpath="//a[normalize-space(text())='CRM/SFA']")
+	WebElement link;
+	
+
+//Action Methods
+	
+	public boolean isWelcomeTextDisplayed() {
+		return welcomeText.isDisplayed();
+	}
+	
+	public boolean logoutButton() {
+		return logoutBtn.isDisplayed();
+	}
+	
+	public boolean crmsfaLink() {
+		return link.isDisplayed();
+	}
+	
+
+}
